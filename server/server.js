dac6fea943f6b0dc926bb96154562fd780c187d2
shresthaabhinav <shresthaabhinav5@gmail.com>
2026-04-12 23:09:08 +0545

    apis created

--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config"
 import multer from "multer";
-import connectDB from "./config/db";
+import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import employeeRouter from "./routes/employeeRoutes.js";
+import profileRouter from "./routes/profileRoutes.js";
+import attendanceRouter from "./routes/attendanceRoutes.js";
 
 const app = express()
 const PORT = process.env.PORT || 4000;
@@ -14,6 +18,10 @@ app.use(multer().none())
 
 // route
 app.get("/", (req, res)=> res.send("Server is running"))
+app.use("/api/auth", authRouter)
+app.use("/api/employees", employeeRouter)
+app.use("/api/profile", profileRouter)
+app.use("/api/attendance", attendanceRouter)
 
 await connectDB()
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
