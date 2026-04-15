639c02cbb0af4cf26e060279589b71a140922ca9
shresthaabhinav <shresthaabhinav5@gmail.com>
2026-04-15 15:10:50 +0545

    payslip api

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () =>{
     try{
-        mongoose.connection.on('conected', ()=> console.log("Database connected"))
+        mongoose.connection.on('connected', ()=> console.log("Database connected"))
         await mongoose.connect(process.env.MONGODB_URI)
     }
     catch(error){
