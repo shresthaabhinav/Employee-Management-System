89a58b7a80b144df4a590a47cb09ef0b1b5b54fa
shresthaabhinav <shresthaabhinav5@gmail.com>
2026-04-16 10:49:18 +0545

    backend completed

---
+++ server/config/nodemailer.js
@@ -0,0 +1,26 @@
+import createTransport from "nodemailer";
+
+const nodemailer = require()
+
+const transporter = createTransport({
+  host: "gmail.com",
+  port: 587,
+  secure: false,
+
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+const sendEmail = async ({to, subject, body}) => {
+    const response = await transporter.sendMail({
+        from: process.env.SENDER_EMAIL,
+        to,
+        subject,
+        html: body
+    })
+    return response;
+}
+
+export default sendEmail
